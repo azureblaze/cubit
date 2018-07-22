01813ce85ab2d6eac2f3ae634466eefeb28633b7
AzureBlaze <azure.blaze@gmail.com>
2018-07-22 16:33:19 -0700

    More tests for EagerSingleton

--- cubittest/cubit/Inject/EagerSingletonsTest.cpp
+++ cubittest/cubit/Inject/EagerSingletonsTest.cpp
@@ -6,30 +6,61 @@ using namespace cubit;
 namespace di = boost::di;
 namespace {
 struct Spy {
-  bool called = false;
+  int called = 0;
 };
 }  // namespace
 
 TEST(EagerSingletonTest, eagerSingleton_created) {
   struct Foo : public EagerSingleton {
-    Foo(Spy& spy) { spy.called = true; }
+    Foo(Spy& spy) { spy.called++; }
   };
 
   auto injector = di::make_injector(di::bind<Foo>().to<Foo>());
 
   InitializeEagerSingletons(injector);
 
-  EXPECT_TRUE(injector.create<Spy&>().called);
+  EXPECT_EQ(1, injector.create<Spy&>().called);
 }
 
 TEST(EagerSingletonTest, noneagerSingleton_notCreated) {
   struct Foo {
-    Foo(Spy& spy) { spy.called = true; }
+    Foo(Spy& spy) { spy.called++; }
   };
 
   auto injector = di::make_injector(di::bind<Foo>().to<Foo>());
 
   InitializeEagerSingletons(injector);
 
-  EXPECT_FALSE(injector.create<Spy&>().called);
+  EXPECT_EQ(0, injector.create<Spy&>().called);
+}
+
+di::injector<Spy&> getParentInjector() {
+  struct Foo : public EagerSingleton {
+    Foo(Spy& spy) { spy.called++; }
+  };
+  auto injector = di::make_injector(di::bind<Foo>().to<Foo>());
+  InitializeEagerSingletons(injector);
+  return injector;
+}
+
+TEST(EagerSingletonTest, eagerSingleton_initializedInParent_created) {
+  auto injector = di::make_injector(getParentInjector());
+
+  EXPECT_EQ(1, injector.create<Spy&>().called);
+}
+
+TEST(EagerSingletonTest, eagerSingleton_initializedInChild_created) {
+  struct BarSpy {
+    int called = 0;
+  };
+  struct Bar : public EagerSingleton {
+    Bar(BarSpy& spy) { spy.called++; }
+  };
+
+  auto injector =
+      di::make_injector(getParentInjector(), di::bind<Bar>().to<Bar>());
+  InitializeEagerSingletons(injector);
+
+  EXPECT_EQ(1, injector.create<Spy&>().called);
+  EXPECT_EQ(1, injector.create<BarSpy&>().called);
 }
